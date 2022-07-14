3da0dfbfdfdc91be8241b5a8652a89bc45081b8e
thantmyat <thantmyat1234@gmail.com>
2022-07-14 20:36:30 +0630

    update upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -69,13 +69,15 @@ const Upload = () => {
 
     return (
         <div className='flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#f8f8f8] justify-center'>
-            <div className='bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6'>
+            {/* <div className='bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6'> */}
+            <div className='bg-white rounded-lg xl:h-[80vh] w-[100%] md:w-[60%] flex gap-6 flex-wrap justify-center md:justify-between items-center p-10 md:p-14 pt-6'>
                 <div>
                     <div>
                         <p className='text-2xl font-bold'>Upload Video</p>
                         <p className='text-md text-gray-400 mt-1'>Post a video to your account</p>
                     </div>
-                    <div className='border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100'>
+                    {/* <div className='border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100'> */}
+                    <div className='border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-2 md:p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100'>
                         {isLoading ? (
                             <p>Uploading...</p>
                         ): (
